e9a31e76ab493e3409bd11a2389b1475d3c1883a
Anthony I <awi7573@rit.edu>
2020-03-02 19:11:33 -0500

    Added automatic time creator

--- GNU_code/Record_ref/scheduler.py
+++ GNU_code/Record_ref/scheduler.py
@@ -35,8 +35,8 @@ def utc2local(utc):
     return utc + offset
 
 if __name__=='__main__':
-    sched = BlockingScheduler()
-    # sched = BackgroundScheduler()
+    # sched = BlockingScheduler()
+    sched = BackgroundScheduler()
 
     Date=[]
     Doppler=[]
