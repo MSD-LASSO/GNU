3d688ef01b3852f3b1300e7447286a7e73e40a6e
Andrew deVries <ard6268@rit.edu>
2020-03-06 19:00:38 -0500

    updated filterDemod_ref

--- GNU_code/FilterDemod_ref/filterDemod_ref.py
+++ GNU_code/FilterDemod_ref/filterDemod_ref.py
@@ -6,19 +6,7 @@
 # GNU Radio version: 3.7.13.5
 ##################################################
 
-if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print "Warning: failed to XInitThreads()"
-
-from PyQt4 import Qt
 from gnuradio import analog
-from gnuradio import audio
 from gnuradio import blocks
 from gnuradio import eng_notation
 from gnuradio import filter
@@ -27,36 +15,12 @@ from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
 from optparse import OptionParser
 import pmt
-import sys
-from gnuradio import qtgui
 
 
-class filterDemod_ref(gr.top_block, Qt.QWidget):
+class filterDemod_ref(gr.top_block):
 
     def __init__(self, cutoff_freq=80000, decimation=1, gain=2, num_samples=10000000, samp_rate=2000000, save_file='', source_file='', trans_width=2000):
         gr.top_block.__init__(self, "Filterdemod Ref")
-        Qt.QWidget.__init__(self)
-        self.setWindowTitle("Filterdemod Ref")
-        qtgui.util.check_set_qss()
-        try:
-            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except:
-            pass
-        self.top_scroll_layout = Qt.QVBoxLayout()
-        self.setLayout(self.top_scroll_layout)
-        self.top_scroll = Qt.QScrollArea()
-        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
-        self.top_scroll_layout.addWidget(self.top_scroll)
-        self.top_scroll.setWidgetResizable(True)
-        self.top_widget = Qt.QWidget()
-        self.top_scroll.setWidget(self.top_widget)
-        self.top_layout = Qt.QVBoxLayout(self.top_widget)
-        self.top_grid_layout = Qt.QGridLayout()
-        self.top_layout.addLayout(self.top_grid_layout)
-
-        self.settings = Qt.QSettings("GNU Radio", "filterDemod_ref")
-        self.restoreGeometry(self.settings.value("geometry").toByteArray())
-
 
         ##################################################
         # Parameters
@@ -73,22 +37,16 @@ class filterDemod_ref(gr.top_block, Qt.QWidget):
         ##################################################
         # Blocks
         ##################################################
-        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
-                interpolation=12,
-                decimation=5,
-                taps=None,
-                fractional_bw=None,
-        )
-        self.low_pass_filter_0 = filter.fir_filter_ccf(10, firdes.low_pass(
-        	1, samp_rate, 110e3, 20e3, firdes.WIN_HAMMING, 6.76))
+        self.low_pass_filter_0_1 = filter.fir_filter_ccf(decimation, firdes.low_pass(
+        	gain, samp_rate, cutoff_freq, trans_width, firdes.WIN_HAMMING, 6.76))
+        self.blocks_wavfile_sink_0 = blocks.wavfile_sink(save_file, 1, samp_rate, 8)
         self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
-        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vff((1, ))
-        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_gr_complex*1, 'C:\\Users\\devri\\Documents\\RIT\\Sixth Semester\\MSD I\\GIT\\GNU_non_git\\2_28_GPS_SECOND_pi1\\Time2020-02-28_19_27_05_113044_END_2020-02-28_19_27_05_935741', True)
+        self.blocks_head_0_0 = blocks.head(gr.sizeof_float*1, num_samples)
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_gr_complex*1, source_file, False)
         self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
-        self.audio_sink_0 = audio.sink(48000, '', True)
-        self.analog_wfm_rcv_0_0 = analog.wfm_rcv(
-        	quad_rate=480e3,
-        	audio_decimation=10,
+        self.analog_wfm_rcv_0 = analog.wfm_rcv(
+        	quad_rate=samp_rate,
+        	audio_decimation=1,
         )
 
 
@@ -96,23 +54,18 @@ class filterDemod_ref(gr.top_block, Qt.QWidget):
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_wfm_rcv_0_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.analog_wfm_rcv_0, 0), (self.blocks_head_0_0, 0))
         self.connect((self.blocks_file_source_0, 0), (self.blocks_throttle_0, 0))
-        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.audio_sink_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.low_pass_filter_0, 0))
-        self.connect((self.low_pass_filter_0, 0), (self.rational_resampler_xxx_0, 0))
-        self.connect((self.rational_resampler_xxx_0, 0), (self.analog_wfm_rcv_0_0, 0))
-
-    def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "filterDemod_ref")
-        self.settings.setValue("geometry", self.saveGeometry())
-        event.accept()
+        self.connect((self.blocks_head_0_0, 0), (self.blocks_wavfile_sink_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.low_pass_filter_0_1, 0))
+        self.connect((self.low_pass_filter_0_1, 0), (self.analog_wfm_rcv_0, 0))
 
     def get_cutoff_freq(self):
         return self.cutoff_freq
 
     def set_cutoff_freq(self, cutoff_freq):
         self.cutoff_freq = cutoff_freq
+        self.low_pass_filter_0_1.set_taps(firdes.low_pass(self.gain, self.samp_rate, self.cutoff_freq, self.trans_width, firdes.WIN_HAMMING, 6.76))
 
     def get_decimation(self):
         return self.decimation
@@ -125,19 +78,21 @@ class filterDemod_ref(gr.top_block, Qt.QWidget):
 
     def set_gain(self, gain):
         self.gain = gain
+        self.low_pass_filter_0_1.set_taps(firdes.low_pass(self.gain, self.samp_rate, self.cutoff_freq, self.trans_width, firdes.WIN_HAMMING, 6.76))
 
     def get_num_samples(self):
         return self.num_samples
 
     def set_num_samples(self, num_samples):
         self.num_samples = num_samples
+        self.blocks_head_0_0.set_length(self.num_samples)
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, 110e3, 20e3, firdes.WIN_HAMMING, 6.76))
+        self.low_pass_filter_0_1.set_taps(firdes.low_pass(self.gain, self.samp_rate, self.cutoff_freq, self.trans_width, firdes.WIN_HAMMING, 6.76))
         self.blocks_throttle_0.set_sample_rate(self.samp_rate)
 
     def get_save_file(self):
@@ -145,18 +100,21 @@ class filterDemod_ref(gr.top_block, Qt.QWidget):
 
     def set_save_file(self, save_file):
         self.save_file = save_file
+        self.blocks_wavfile_sink_0.open(self.save_file)
 
     def get_source_file(self):
         return self.source_file
 
     def set_source_file(self, source_file):
         self.source_file = source_file
+        self.blocks_file_source_0.open(self.source_file, False)
 
     def get_trans_width(self):
         return self.trans_width
 
     def set_trans_width(self, trans_width):
         self.trans_width = trans_width
+        self.low_pass_filter_0_1.set_taps(firdes.low_pass(self.gain, self.samp_rate, self.cutoff_freq, self.trans_width, firdes.WIN_HAMMING, 6.76))
 
 
 def argument_parser():
@@ -192,21 +150,9 @@ def main(top_block_cls=filterDemod_ref, options=None):
     if options is None:
         options, _ = argument_parser().parse_args()
 
-    from distutils.version import StrictVersion
-    if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
-        style = gr.prefs().get_string('qtgui', 'style', 'raster')
-        Qt.QApplication.setGraphicsSystem(style)
-    qapp = Qt.QApplication(sys.argv)
-
     tb = top_block_cls(cutoff_freq=options.cutoff_freq, decimation=options.decimation, gain=options.gain, num_samples=options.num_samples, samp_rate=options.samp_rate, save_file=options.save_file, source_file=options.source_file, trans_width=options.trans_width)
     tb.start()
-    tb.show()
-
-    def quitting():
-        tb.stop()
-        tb.wait()
-    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
-    qapp.exec_()
+    tb.wait()
 
 
 if __name__ == '__main__':
