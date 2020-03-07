9ef09f02e72205e950a59a80cbd09a512ee4fd11
Anthony I <awi7573@rit.edu>
2020-03-06 20:59:48 -0500

    os version call of Filter

--- GNU_code/FilterDemod_ref/filterReader.py
+++ GNU_code/FilterDemod_ref/filterReader.py
@@ -1,6 +1,7 @@
 import filterDemod_ref
 from os import walk
 from os import mkdir
+import os
 
 def main(top=filterDemod_ref, options=None):
     directoryToFiles = "../../../../3_6_refSatref_test2/"
@@ -17,19 +18,23 @@ def main(top=filterDemod_ref, options=None):
             files.extend(filenames)
             break
 
-        print(files)
+        # print(files)
         j=0
         while(j<len(files)):
             inputFile=files[j]
             if ~inputFile.__contains__("hdr"):
-
+                inputPath=directoryToFiles+'/pi'+str(i+1)+'/'+inputFile
                 outputName = directoryToFiles+'/pi'+str(i+1)+'_filtered/'+inputFile+".wav"
-                print(outputName)
-                tb = top(cutoff_freq=cutoff_freq, samp_rate=sampleRate, save_file=outputName,
-                         source_file=inputFile, trans_width=2000)
-                tb.start()
-                tb.wait()
-                del tb
+                # print(outputName)
+
+                # tb = top(cutoff_freq=cutoff_freq, samp_rate=sampleRate, save_file=outputName,
+                #          source_file=inputFile, trans_width=2000)
+                # tb.start()
+                # tb.wait()
+                # del tb
+                String='python filterDemod_ref.py --save-file='+outputName+' --source-file='+inputPath+' --cutoff-freq='+str(cutoff_freq)+' --samp-rate='+str(sampleRate)+'trans_width=2000'
+                print(String)
+                os.system(String)
             j+=1
 
 if __name__ == '__main__':
