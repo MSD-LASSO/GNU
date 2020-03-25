76165541f5d1203208393b84c67b968090953940
Anthony I <awi7573@rit.edu>
2020-03-24 20:04:17 -0400

    Offline test

--- GNU_code/Record_ref/CreateTimes.py
+++ GNU_code/Record_ref/CreateTimes.py
@@ -47,7 +47,7 @@ def argument_parser():
     # Notice timedelta is 4 hours during daylight savings time
     # Eastern Standard Time is 5 hours.
     parser.add_option(
-        "", "--startTime", dest="startTime", type="string", default=str(dt.now().date())+'T'+str((dt.now()+bigDT.timedelta(hours=4,minutes=3)).time()),
+        "", "--startTime", dest="startTime", type="string", default=str(dt.now().date())+'T'+str((dt.now()+bigDT.timedelta(days=1,hours=4,minutes=3)).time()),
         help="Set DateTime in form 'YY-MM-DDTHH-MM-SS.fff' [default=%default]")
     parser.add_option(
         "", "--delay", dest="delay", type="float", default=20,
