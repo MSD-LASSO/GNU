60acf91b0cc6392c3a5ac61ac66f5a87dc95e760
Anthony I <awi7573@rit.edu>
2020-04-01 13:53:46 -0400

    Added a test script for scheduler_Runner.py

--- GNU_code/Record_ref/Tester.py
+++ GNU_code/Record_ref/Tester.py
@@ -50,26 +50,31 @@ def runTest(fileDirectory,scheduler,hackrf_index,schedulerFile):
         files.extend(filenames)
         break
 
+    print("Checking Number of Files")
     # Data file and header file for each numEntries and the debugger.
     assert files.__len__() == numEntries*2+1
+    print("Number of files is correct.")
 
+    print("Checking File Sizes")
     for currentFile in files:
         # Check file sizes for data sets. Ignore header files.
         if currentFile.__contains__(".hdr")==0 and currentFile.__contains__("Debugger")==0:
             print(fileDirectory+currentFile)
             print(os.stat(fileDirectory+currentFile).st_size)
             assert os.stat(fileDirectory+currentFile).st_size==80e6
-
+    print("All file sizes are correct")
     # os.stat_result(st_mode=33188, st_ino=6419862, st_dev=16777220, st_nlink=1, st_uid=501, st_gid=20, st_size=1564,
     #                st_atime=1584299303, st_mtime=1584299400, st_ctime=1584299400)
 
     # Clean up.
+    print("Cleaning up.....")
     for currentFile in files:
         os.remove(fileDirectory+currentFile)
+    print("All recordings deleted.")
 
     if deleteDirectories==1:
         os.rmdir(fileDirectory)
-
+    print("FileDirectory deleted. Test completed.")
 
 
 if __name__ == '__main__':
@@ -83,5 +88,8 @@ if __name__ == '__main__':
     print("This script WILL take ~10 minutes to run with intermediate progress reports.")
     print("This script WILL OVERWRITE the following: InputTimes.txt")
     basicTest()
+    print("Basic Test completed. 75% complete. Starting next Test...")
     allScheduledTimesAlreadyHappenedTest()
+    print("Scheduled Times already happened test completed. 87.5% complete. Starting next Test...")
     noHackrfPluggedInTest()
+    print("All tests complete")
