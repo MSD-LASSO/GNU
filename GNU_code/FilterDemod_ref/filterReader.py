3a7a44388f31bcd22e8b70baac1e7752b1f4d9d0
Anthony I <awi7573@rit.edu>
2020-04-01 21:13:00 -0400

    MAde some updates to filterReader.py

--- GNU_code/FilterDemod_ref/filterReader.py
+++ GNU_code/FilterDemod_ref/filterReader.py
@@ -1,20 +1,26 @@
-import filterDemod_ref
 from os import walk
 from os import mkdir
 import os
 
-def main(top=filterDemod_ref, options=None):
-    # Directory to find the source files. This must be changed every time.
+# filterReader will automatically filter signals using GNUradio program filterDemod_ref.
+
+def main():
+    # Path to directory to find the source files. This must be changed every time.
+    # The source files should be organized by pi. So under hailMaryLUSAT, there'd be directories pi1 and pi2.
+    # NOTE: ../ means go up 1 directory.
     directoryToFiles = "../../../../hailMaryLUSAT/"
 
     # Change the sample rate from 2million if applicable
     sampleRate = 2000000
-    # cutoff_Freq is how wide the filter is. Narrower for narrower signals?
-    # cutoff_freq = 80000
-    cutoff_freq = 5000
 
+    # cutoff_Freq is how wide the filter is. Narrower for narrower signals?
+    # cutoff_freq = 80000 #For reference signals
+    cutoff_freq = 5000 #For satellite signals
+
+    # Iterate once for each pi.
     for i in range(2):
-        # Make the output directories
+        # Make the output directories pi1_filtered, pi2_filtered only if they don't already exist. This program WILL
+        #overwrite existing items in this directory if names match. (happens if you filter back to back, for example).
         if os.path.exists(directoryToFiles+'/pi'+str(i+1)+'_filtered/')==0:
             mkdir(directoryToFiles+'/pi'+str(i+1)+'_filtered/')
 
@@ -25,25 +31,25 @@ def main(top=filterDemod_ref, options=None):
             files.extend(filenames)
             break
 
-        # print(files)
+        # print(files) #debugging tool.
+
         # Iterate through all files in the source directory. If the file is not a header file or a debugger file as
         # identified by "hdr" and "Debugger", then we filter that file.
         # We specify the input and outputh paths and run GNUradio filterDemod_ref.py
         print("past pi_filtered folder creation")
         j=0
         while(j<len(files)):
+            # For each file in the directory, get the name of the file.
             inputFile=files[j]
+            # If the file does NOT contain hdr (is a header file) or Debugger (is the debugger file) then
+            # we filter it.
             if inputFile.__contains__("hdr")==0 & inputFile.__contains__("Debugger")==0:
                 inputPath=directoryToFiles+'/pi'+str(i+1)+'/'+inputFile
                 outputName = directoryToFiles+'/pi'+str(i+1)+'_filtered/'+inputFile+".wav"
                 print("before calling GNU to filter signal")
-                # print(outputName)
 
-                # tb = top(cutoff_freq=cutoff_freq, samp_rate=sampleRate, save_file=outputName,
-                #          source_file=inputFile, trans_width=2000)
-                # tb.start()
-                # tb.wait()
-                # del tb
+                # This is exactly how one would call filterDemod_ref.py from the command line.
+                # NOTE: you must be in the directory containing filterDemod_ref.py
                 String='python filterDemod_ref.py --save-file='+outputName+' --source-file='+inputPath+' --cutoff-freq='+str(cutoff_freq)+' --samp-rate='+str(sampleRate)+' --trans-width=2000'
                 print(String)
                 os.system(String)
